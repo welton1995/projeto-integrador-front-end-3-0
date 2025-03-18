568c496a3d187c6042ea7a294bd8486157c37ad4
welton1995 <estudanteativo198@gmail.com>
2025-03-17 21:53:47 -0300

    update

--- js/verficaToken.js
+++ js/verficaToken.js
@@ -1,25 +1,31 @@
-document.addEventListener("DOMContentLoaded", async ()=> {
+document.addEventListener("DOMContentLoaded", async () => {
   const token = localStorage.getItem("token");
-  
+
   if (!token) {
     await Swal.fire({
       title: "Usuário não autorizado!",
       icon: "info",
       confirmButtonColor: "#0275d8",
     });
-      window.location.href = "../index.html";
+    window.location.href = "../index.html";
   }
 });
 
-const sair  = document.querySelector('#sair');
 
-sair.addEventListener('click', async (event)=> {
+setInterval(() => {
+  localStorage.clear(); 
+  window.location.reload();
+}, 3 * 60 * 60 * 1000); 
+
+const sair = document.querySelector("#sair");
+
+sair.addEventListener("click", async (event) => {
   event.preventDefault();
 
   localStorage.removeItem("token");
   localStorage.removeItem("user");
 
-  setTimeout(()=> {
-    window.location.href='../index.html'
+  setTimeout(() => {
+    window.location.href = "../index.html";
   }, 80);
 });
